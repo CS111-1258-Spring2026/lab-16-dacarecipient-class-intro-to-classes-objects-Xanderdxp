2e84576941e300aeb7cea0fa06f4cbe18de6dafb
Xanderdxp <xerozion@gmail.com>
2026-05-26 18:34:50 +0000

    1.0

--- DACArecipient.java
+++ DACArecipient.java
@@ -20,14 +20,72 @@
 public class DACArecipient
 {
 	/***** INSTANCE VARIABLES *****/
-	//TODO: Declare the instance variables. 
+	private String surname, givenName, uscisNumber, countryOfOrigin;
+	private int birthday, validFromDate, expirationDate;
+	private char sex;
 
-	/***** ACCESSORS *****/
-	//TODO: Write the getter for each instance variable. Remember to include documentation for each method.
 	
-	/***** MUTATORS *****/
-	//TODO: Write the setter for each instance variable. Remember to include documentation for each method.
+	/***** ACCESSORS *****/ //getters
+	public String getSurname(){
+		return this.surname;
+	}
+	public String getGivenName(){
+		return this.givenName;
+	}
+	public String getUscisNumber(){
+		return this.uscisNumber;
+	}
+	public String getCountryOfOrigin(){
+		return this.countryOfOrigin;
+	}
+	public int getBirthday(){
+		return this.birthday;
+	}
+	public int getValidFromDate(){
+		return this.validFromDate;
+	}
+	public int getExpirationDate(){
+		return this.expirationDate;
+	}
+	public char getSex(){
+		return this.sex;
+	}
 
-	//TODO: Write the setAll method. Remember to include documentation.
 
+	/***** MUTATORS *****/ //setters
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	public void setGivenName(String givenName){
+		this.givenName = givenName;
+	}
+	public void setUscisNumber(String uscisNumber){
+		this.uscisNumber = uscisNumber;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin){
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public void setBirthday(int birthday){
+		this.birthday = birthday;
+	}
+	public void setValidFromDate(int validFromDate){
+		this.validFromDate = validFromDate;
+	}
+	public void setExpirationDate(int expirationDate){
+		this.expirationDate = expirationDate;
+	}
+	public void setSex(char sex){
+		this.sex = sex;
+	}
+	
+	public void setAll(String surname, String givenName, String uscisNumber, String countryOfOrigin, int birthday, int validFromDate, int expirationDate, char sex){
+		this.surname = surname;
+		this.givenName = givenName;
+		this.uscisNumber = uscisNumber;
+		this.countryOfOrigin = countryOfOrigin;
+		this.birthday = birthday;
+		this.validFromDate = validFromDate;
+		this.expirationDate = expirationDate;
+		this.sex = sex;
+	}
 }
--- Main.java
+++ Main.java
@@ -14,6 +14,40 @@ class Main
 {
   public static void main(String[] args) 
 	{
-    //TODO PART 3: test your setter/getter here!
+    
+
+    //Person 1: Using individual setters
+    DACArecipient person1 = new DACArecipient();
+    person1.setSurname("Mendez");
+    person1.setGivenName("Javier");
+    person1.setUscisNumber("56-3-445");
+    person1.setCountryOfOrigin("Venezuela");
+    person1.setBirthday(2451564);
+    person1.setValidFromDate(3956753);
+    person1.setExpirationDate(3956840);
+    person1.setSex('M');
+    
+    //Person 2: Using the setAll method
+    DACArecipient person2 = new DACArecipient();
+    person2.setAll("Chapeton-Lamas", "Nery", "12-4-789" , "Guatemala", 2451564, 3000000, 4000000, 'M');
+  
+    //Print results using getters
+    printRecipient(person1,"Person 1");
+    printRecipient(person2, "Person 2");
   }
+
+    public static void printRecipient(DACArecipient p, String label) {
+      System.out.println("\n" + label);
+      System.out.println("-----------------------------");
+      System.out.println("Surname: " + p.getSurname());
+      System.out.println("Given name: " + p.getGivenName());
+      System.out.println("USCIS number: " + p.getUscisNumber());
+      System.out.println("Country of Origin: " + p.getCountryOfOrigin());
+      System.out.println("Birthday(JDN): " + p.getBirthday());
+      System.out.println("Valid From(JDN): " + p.getValidFromDate());
+      System.out.println("Expiration(JDN): " + p.getExpirationDate());
+      System.out.println("Sex: " + p.getSex());
+    
+  
+    }
 }
